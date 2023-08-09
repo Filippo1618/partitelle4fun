edec85cf8c3bc7b7a570129fe87ae274e5d7716e
Filippomaria <filippomaria.galimberti.rossi@gmail.com>
2023-08-08 17:24:20 -0700

    disaccoppiamento pagine html

--- partitelle/models.py
+++ partitelle/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Giocatore(models.Model):
     nome = models.CharField(max_length=40, default='*')
     descrizione = models.CharField(max_length=450, default='*')
+    
     portiere= models.BooleanField(default=False)
     difensore = models.BooleanField(default=False)
     ala_sinistra= models.BooleanField(default=False)
